3899ea6d31d6037b944b0fdf616812208e2cfb33
sereyvathna21 <vattdy192@gmail.com>
2025-03-19 14:38:01 +0700

    first commit

---
+++ GEN-09-FLUTTER-2-BLA-W7-START/lib/repository/mock/mock_locations_repository.dart
@@ -0,0 +1,27 @@
+import '../../model/location/locations.dart';
+import '../locations_repository.dart';
+
+class MockLocationsRepository extends LocationsRepository {
+  const Location phnomPenh = Location(
+    name: "Phnom Penh",
+    country: Country.cambodia,
+  );
+  const Location siemReap = Location(
+    name: "Siem Reap",
+    country: Country.cambodia,
+  );
+  const Location sihanoukville = Location(
+    name: "Sihanoukville",
+    country: Country.cambodia,
+  );
+  const Location kampot = Location(name: "Kampot", country: Country.cambodia);
+  const Location battambang = Location(
+    name: "Battambang",
+    country: Country.cambodia,
+  );
+
+  @override
+  List<Location> getLocations() {
+    return [phnomPenh, siemReap, sihanoukville, kampot, battambang];
+  }
+}
