200d2818c0c9b2cd4ac33029cbf3d84b828c348e
sereyvathna21 <vattdy192@gmail.com>
2025-03-31 17:40:19 +0700

    week 8

--- lib/PRACTICE - START-CODE/EX-1-START-CODE/Repository/course_repository.dart
+++
@@ -1,7 +0,0 @@
-import '../models/course.dart';
-import 'mock_repository.dart';
-
-List<Course> getCourses() => getCoursesFromMock();
-
-void addScore(Course course, CourseScore score) =>
-    addScoreToMock(course, score);
--- lib/PRACTICE - START-CODE/EX-2-START-CODE/repository/post_repository.dart
+++
@@ -1,6 +0,0 @@
-import '../model/post.dart';
-
-abstract class PostRepository {
-  Future<Post> getPost(int postId);
-  Future<List<Post>> getPosts();
-}
